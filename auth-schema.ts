0c0217cd4839dcd5f7ff99dc1ea4764a67b89082
Bart <bart@Barts-MacBook-Pro.local>
2026-01-21 22:33:52 +0100

    Now users are singed up with Ops role as default

--- auth-schema.ts
+++ auth-schema.ts
@@ -12,7 +12,7 @@ export const user = pgTable("user", {
     .defaultNow()
     .$onUpdate(() => /* @__PURE__ */ new Date())
     .notNull(),
-  role: text("role"),
+  role: text("role", { enum: ["MIM", "Ops", "Business"] }).default("Ops"),
   banned: boolean("banned").default(false),
   banReason: text("ban_reason"),
   banExpires: timestamp("ban_expires"),
