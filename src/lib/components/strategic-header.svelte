ce19ac169d4ccc8ea438cfacd114333ac24f4389
Bart <bart@Barts-MacBook-Pro.local>
2026-01-21 20:05:52 +0100

    strategic header draft done

--- src/lib/components/strategic-header.svelte
+++ src/lib/components/strategic-header.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <section
-	class="flex justify-center gap-6 rounded-xl border border-white bg-background-panel p-6 shadow-lg"
+	class="border-DEFAULT gap-6 rounded-xl border bg-background-panel p-6 shadow-lg md:flex md:justify-center"
 >
 	<div class="gap-3">
 		<p class="flex min-w-2xs items-center gap-3">
@@ -14,7 +14,7 @@
 		<h1 class="text-text-primary">INC-31234231: API Latency</h1>
 		<p class="text-text-secondary">Impact: HIGH 100%</p>
 	</div>
-	<div class="gap-6 border-l py-2 pl-6">
+	<div class="gap-6 py-2 pl-6 md:border-l">
 		<p class="text-xs text-text-secondary">Duration</p>
 		<p class="text-4xl text-text-primary">00:40:15</p>
 		<div class="text-xs">
@@ -24,7 +24,7 @@
 			</p>
 		</div>
 	</div>
-	<div class=" gap-6 border-l py-2 pl-6">
+	<div class=" gap-6 py-2 pl-6 md:border-l">
 		<p class="text-xs text-text-secondary">Current Status:</p>
 		<p class="text-4xl text-text-primary">OPEN</p>
 	</div>
