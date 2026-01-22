90e57b1bf5449b1e450a9951f95740b4656536b4
Bart <bart@Barts-MacBook-Pro.local>
2026-01-22 07:55:57 +0100

    small changes to login page were done

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
 	import { login, signup } from '$lib/api/auth.remote';
 
-	let action: 'login' | 'signup' = $state('login');
+	let action: 'Login' | 'Signup' = $state('Login');
 
-	let func = $derived.by(() => (action === 'login' ? login : signup));
+	let func = $derived.by(() => (action === 'Login' ? login : signup));
 </script>
 
-<section>
-	<div class="bg-background-panel">
-		<form {...func} class="flex flex-col gap-4 p-4">
-			{#if action === 'signup'}
+<section class="flex h-screen items-center justify-center">
+	<div class="rounded-lg bg-background-panel p-6 md:w-[400px]">
+		<h1 class="mb-6 text-2xl font-bold">{action === 'Login' ? 'Welcome Back' : 'Welcome'}</h1>
+		<form {...func} class="flex flex-col gap-4">
+			{#if action === 'Signup'}
 				<label for="name"> Name: </label>
 				<input
 					id="name"
 					{...signup.fields.name.as('text')}
-					class="border-DEFAULT border-subtle rounded-md border"
+					class="border-DEFAULT border-subtle rounded-lg border"
 				/>
 				{#each signup.fields.name.issues() as issue}
 					<p class="text-status-critical">{issue.message}</p>
@@ -25,7 +26,7 @@
 			<input
 				id="email"
 				{...func.fields.email.as('text')}
-				class="border-DEFAULT border-subtle rounded-md border"
+				class="border-DEFAULT border-subtle rounded-lg border"
 			/>
 			{#each func.fields.email.issues() as issue}
 				<p class="text-status-critical">{issue.message}</p>
@@ -34,17 +35,20 @@
 			<input
 				id="password"
 				{...func.fields.password.as('text')}
-				class="border-DEFAULT border-subtle rounded-md border"
+				class="border-DEFAULT border-subtle rounded-lg border"
 			/>
 			{#each func.fields.password.issues() as issue}
 				<p class="text-status-critical">{issue.message}</p>
 			{/each}
 			<div class="ml-auto inline-block p-4">
-				<button class="rounded-md bg-button-primary p-2 text-text-primary">{action}</button>
+				<button class="rounded-lg bg-button-primary p-2 text-text-primary">{action}</button>
 			</div>
 		</form>
-		<button onclick={() => (action = action === 'login' ? 'signup' : 'login')}>
-			Switch to {action === 'login' ? 'signup' : 'login'}
+		<button
+			class="mt-4 ml-auto block text-text-secondary"
+			onclick={() => (action = action === 'Login' ? 'Signup' : 'Login')}
+		>
+			{action === 'Login' ? 'I dont have an account' : 'login'}
 		</button>
 	</div>
 </section>
